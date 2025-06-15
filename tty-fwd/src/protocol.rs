397b5803acd96b113b819f32d06ecd96395e8b24
Armin Ronacher <armin.ronacher@active-4.com>
2025-06-15 21:34:32 +0200

    Add the protocol definitions from asciinema

--- tty-fwd/src/protocol.rs
+++ tty-fwd/src/protocol.rs
@@ -1,8 +1,58 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 #[derive(Serialize)]
 pub struct SessionInfo {
     pub cmdline: Vec<String>,
     pub name: String,
     pub cwd: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AsciinemaHeader {
+    pub version: u32,
+    pub width: u32,
+    pub height: u32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub timestamp: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub duration: Option<f64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub command: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub title: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub env: Option<HashMap<String, String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub theme: Option<AsciinemaTheme>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AsciinemaTheme {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub fg: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub bg: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub palette: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "type")]
+pub enum AsciinemaEventType {
+    #[serde(rename = "o")]
+    Output,
+    #[serde(rename = "i")]
+    Input,
+    #[serde(rename = "m")]
+    Marker,
+    #[serde(rename = "r")]
+    Resize,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AsciinemaEvent {
+    pub time: f64,
+    pub event_type: AsciinemaEventType,
+    pub data: String,
 }
