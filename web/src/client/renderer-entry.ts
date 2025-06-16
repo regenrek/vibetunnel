e4b4a37c86b30048e597aa2daf14e298ef20dbb0
Mario Zechner <badlogicgames@gmail.com>
2025-06-16 09:38:37 +0200

    Fix renderer export name - use TerminalRenderer with Renderer alias

--- web/src/client/renderer-entry.ts
+++ web/src/client/renderer-entry.ts
@@ -1,3 +1,6 @@
 // Entry point for renderer bundle - exports both renderers for tests
-export { Renderer } from './renderer.js';
+export { TerminalRenderer } from './renderer.js';
 export { XTermRenderer } from './xterm-renderer.js';
+
+// Also export with shorter alias for convenience
+export { TerminalRenderer as Renderer } from './renderer.js';
