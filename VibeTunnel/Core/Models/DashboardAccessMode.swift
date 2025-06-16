f56cef3ad3a030df91698f890ba6d7b021b16787
Peter Steinberger <steipete@gmail.com>
2025-06-16 20:29:20 +0200

    Add access mode for settings

---
+++ VibeTunnel/Core/Models/DashboardAccessMode.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+/// Dashboard access mode
+enum DashboardAccessMode: String, CaseIterable {
+    case localhost = "localhost"
+    case network = "network"
+    
+    var displayName: String {
+        switch self {
+        case .localhost: "Localhost only"
+        case .network: "Network"
+        }
+    }
+    
+    var bindAddress: String {
+        switch self {
+        case .localhost: "127.0.0.1"
+        case .network: "0.0.0.0"
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .localhost: "Only accessible from this Mac"
+        case .network: "Accessible from other devices on the network"
+        }
+    }
+}
