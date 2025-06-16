aadc65f2cc35cc74bdb8f93bfa40c71c4b0ab9c8
Peter Steinberger <steipete@gmail.com>
2025-06-17 01:38:42 +0200

    remove dupe

--- VibeTunnel/Presentation/Views/AboutView.swift
+++ VibeTunnel/Presentation/Views/AboutView.swift
@@ -199,37 +199,6 @@ struct InteractiveAppIcon: View {
     }
 }
 
-// MARK: - Credit Link Component
-
-/// Credit link component for individual contributors.
-///
-/// This component displays a contributor's handle as a clickable link
-/// that opens their website when clicked.
-struct CreditLink: View {
-    let name: String
-    let url: String
-    @State private var isHovering = false
-
-    var body: some View {
-        Button(action: {
-            if let linkURL = URL(string: url) {
-                NSWorkspace.shared.open(linkURL)
-            }
-        }, label: {
-            Text(name)
-                .font(.caption)
-                .underline(isHovering, color: .accentColor)
-        })
-        .buttonStyle(.link)
-        .pointingHandCursor()
-        .onHover { hovering in
-            withAnimation(.easeInOut(duration: 0.2)) {
-                isHovering = hovering
-            }
-        }
-    }
-}
-
 // MARK: - Preview
 
 #Preview("About View") {
