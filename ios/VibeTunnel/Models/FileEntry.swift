a56682f44c31c1fad117281ea75dc6488e3a9919
Peter Steinberger <steipete@gmail.com>
2025-06-20 07:39:54 +0200

    make file browser compat

--- ios/VibeTunnel/Models/FileEntry.swift
+++ ios/VibeTunnel/Models/FileEntry.swift
@@ -19,6 +19,31 @@ struct FileEntry: Codable, Identifiable {
         case modTime = "mod_time"
     }
     
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        path = try container.decode(String.self, forKey: .path)
+        isDir = try container.decode(Bool.self, forKey: .isDir)
+        size = try container.decode(Int64.self, forKey: .size)
+        mode = try container.decode(String.self, forKey: .mode)
+        
+        // Decode mod_time string as Date
+        let modTimeString = try container.decode(String.self, forKey: .modTime)
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        if let date = formatter.date(from: modTimeString) {
+            modTime = date
+        } else {
+            // Fallback without fractional seconds
+            formatter.formatOptions = [.withInternetDateTime]
+            if let date = formatter.date(from: modTimeString) {
+                modTime = date
+            } else {
+                throw DecodingError.dataCorruptedError(forKey: .modTime, in: container, debugDescription: "Invalid date format")
+            }
+        }
+    }
+    
     var formattedSize: String {
         let formatter = ByteCountFormatter()
         formatter.countStyle = .binary
